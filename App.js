f2eea692250962fa21ffce3d97896517861bf0e2
Nicholas Husin <nicholashusin@gmail.com>
2018-11-28 21:21:23 -0600

    Added labeling for words.

--- App.js
+++ App.js
@@ -9,11 +9,13 @@ export default class setting extends Component {
   }
 
   words = []
+  labels = []
+  response = ''
 
   add = () => {
-    constructor();
     if (this.state.text !== '') {
       this.words.push(this.state.text)
+      this.labels.push("N")
     }
     this.state.text = ""
     this.textInput.clear()
@@ -23,14 +25,62 @@ export default class setting extends Component {
 
 
   hint = () => {
+    console.log(JSON.stringify(this.words))
+    console.log(JSON.stringify(this.labels))
     this.words = []
+    this.labels = []
     this.forceUpdate()
-    Alert.alert('API Not Connected Yet')
+    Alert.alert('API Was Called')
+  }
+
+  connect() {
+    fetch('http://10.195.23.233:5000', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        words: this.words,
+        labels: this.labels
+      }),
+    })
+    .then(response => response.json())
+    .then(json => console.log(json))
+  }
+
+  labelChange(index) {
+    if (this.labels[index] === "N") {
+      this.labels[index] = "B";
+    }
+    else if (this.labels[index] === "B") {
+      this.labels[index] = "R";
+    }
+    else if (this.labels[index] === "R") {
+      this.labels[index] = "A";
+    }
+    else if (this.labels[index] === "A") {
+      this.labels[index] = "N";
+    }
+    this.forceUpdate()
+  }
+
+  wordBackground(index) {
+    if (this.labels[index] === "B") {
+      return styles.Blue;
+    }
+    if (this.labels[index] === "R") {
+      return styles.Red;
+    }
+    if (this.labels[index] === "A") {
+      return styles.Assassin;
+    }
+    if (this.labels[index] === "N") {
+      return styles.Neutral;
+    }
   }
 
   render() {
-    var wordArr = this.words
-
     return (
       <KeyboardAvoidingView style={{flex: 1}} behavior="padding" enabled>
 
@@ -40,9 +90,16 @@ export default class setting extends Component {
         </View>
 
         {/* List of inputted words */}
-        <ScrollView style={{flex: 1, flexDirection: 'column', backgroundColor:'white'}}>
-          { wordArr.map((item, key)=>(
-            <Text key={key} style={styles.TextStyle}> { item } </Text>)
+        <ScrollView style={{flex: 1, flexDirection: 'column', backgroundColor:'white'}}
+          ref={ref => this.scrollView = ref}
+          onContentSizeChange={(contentWidth, contentHeight)=>{
+            this.scrollView.scrollToEnd({animated: true});
+        }}>
+          {this.words.map((item, key)=>(
+            <Text key={key} onPress={() => {this.labelChange(key)}}
+            style={this.wordBackground(key)}>
+              {key + 1 + ". " + item}
+            </Text>)
           )}
         </ScrollView>
 
@@ -93,13 +150,59 @@ const styles = StyleSheet.create({
       margin: 10
     },
 
-    TextStyle:{
+    Blue: {
       fontFamily: 'monospace',
       fontSize : 18,
       fontWeight: 'bold',
       textAlign: 'center',
-      textShadowColor:'#585858',
-      textShadowOffset:{width: 5, height: 5},
-      textShadowRadius:100,
+      backgroundColor: '#1E90FF',
+      color: 'white',
+      borderRadius: 20,
+      padding: 3,
+      marginTop: 7,
+      marginRight: 15,
+      marginLeft: 15
+    },
+
+    Red: {
+      fontFamily: 'monospace',
+      fontSize : 18,
+      fontWeight: 'bold',
+      textAlign: 'center',
+      backgroundColor: '#B22222',
+      color: 'white',
+      borderRadius: 20,
+      padding: 3,
+      marginTop: 7,
+      marginRight: 15,
+      marginLeft: 15
+    },
+
+    Assassin: {
+      fontFamily: 'monospace',
+      fontSize : 18,
+      fontWeight: 'bold',
+      textAlign: 'center',
+      backgroundColor: '#585858',
+      color: 'white',
+      borderRadius: 20,
+      padding: 3,
+      marginTop: 7,
+      marginRight: 15,
+      marginLeft: 15
+    },
+
+    Neutral: {
+      fontFamily: 'monospace',
+      fontSize : 18,
+      fontWeight: 'bold',
+      textAlign: 'center',
+      backgroundColor: '#cdb79e',
+      color: 'white',
+      borderRadius: 20,
+      padding: 3,
+      marginTop: 7,
+      marginRight: 15,
+      marginLeft: 15
     }
 });
